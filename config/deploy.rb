2f1e289accf7449258e69e0680ce2f24c062bc47
ryan <ryan@sugarworklabs.com>
2014-09-13 00:59:50 -0700

    More changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,18 +63,18 @@ end
 
 namespace :unicorn do
   desc "Zero-downtime restart of Unicorn"
-  task :restart, except: { no_release: true } do
-    run "kill -s USR2 `cat /tmp/unicorn.[application's name].pid`"
+  task :restart do
+    run "kill -s USR2 `cat /tmp/unicorn.ryan.pid`"
   end
 
   desc "Start unicorn"
-  task :start, except: { no_release: true } do
+  task :start  do
     run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
   end
 
   desc "Stop unicorn"
-  task :stop, except: { no_release: true } do
-    run "kill -s QUIT `cat /tmp/unicorn.[application's name].pid`"
+  task :stop do
+    run "kill -s QUIT `cat /tmp/unicorn.ryan.pid`"
   end
 end
 
